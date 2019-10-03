05274084131b24625980df43ed351aadbaf2645c
tvo28 <tvo28@dxc.com>
2019-10-03 14:24:13 +0700

    1 file added

---
+++ src/app/userpref/userpref.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-pref',
+  templateUrl: './userpref.component.html',
+  styleUrls: [ './userpref.component.css' ]
+})
+export class UserPrefComponent  {
+  name = 'UserPrefComponent';
+}
