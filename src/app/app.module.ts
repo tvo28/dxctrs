946c69cb61831ab928c028dbf4380d0056ee8d99
tvo28 <tvo28@dxc.com>
2019-10-03 14:50:14 +0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserPrefComponent } from './userpref/userpref.component';
 
+const appRoutes: Routes = [
+  { path: 'user-pref', component: UserPrefComponent },
+  { path: '', redirectTo: '/user-pref', pathMatch: 'full'}
+];
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule, FormsModule ],
   declarations: [ AppComponent, UserPrefComponent ],
   bootstrap:    [ AppComponent ]
 })
