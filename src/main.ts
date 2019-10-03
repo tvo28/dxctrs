2733fe2faa48cf2ad66544c1563f23179036ae96
tvo28 <tvo28@dxc.com>
2019-10-03 15:25:50 +0700

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+platformBrowserDynamic().bootstrapModule(AppModule);
+/*
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
@@ -14,3 +16,4 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
 
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
+*/
